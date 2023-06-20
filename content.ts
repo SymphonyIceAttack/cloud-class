8a5011ef7f5a03e4d10e8702fe75ba1a64d9b335
SymphonyIceAttack <1994370030@qq.com>
2023-06-20 14:56:18 +0800

    createSocreButton

--- content.ts
+++ content.ts
@@ -10,6 +10,8 @@ window.addEventListener("load", () => {
   // 调用函数以启动MutationObserver
   configureMutationObserver()
   checkIfShouldApplyShadow() && applyShadowToExperienceSpans()
+
+  creatSorceButton()
 })
 
 function configureMutationObserver() {
@@ -63,9 +65,8 @@ function applyShadowToExperienceSpans() {
       }
 
       if (!foundParticipated) {
-        ;(
-          imgs[i].parentNode as HTMLBaseElement
-        ).style.boxShadow = "inset 0 0 5px 5px rgba(255, 0, 0, 0.5)"
+        ;(imgs[i].parentNode as HTMLBaseElement).style.boxShadow =
+          "inset 0 0 5px 5px rgba(255, 0, 0, 0.5)"
       }
     }
   }
@@ -90,4 +91,76 @@ function checkIfShouldApplyShadow() {
   }
 
   return false
+}
+
+function openSpanMenu() {
+  // 获取页面中的所有<span>元素
+  let spanElements = document.getElementsByTagName("span")
+
+  // 迭代遍历每个<span>元素
+  for (let i = 0; i < spanElements.length; i++) {
+    let spanElement = spanElements[i]
+
+    // 检查<span>元素是否具有所需的属性和类
+    let appraiseStatus = spanElement.getAttribute("data-appraise-status")
+    let classNames = spanElement.getAttribute("class")
+
+    // 检查前一个<span>元素的内容是否为"请评分"
+    if (i > 0 && spanElements[i - 1].textContent.trim() === "请评分") {
+      // 执行相应的点击操作
+      spanElement.click()
+    }
+  }
+
+  // 获取滚动条所在的元素，通常是<body>或者指定的容器元素
+  let container = document.documentElement || document.body
+
+  // 将滚动条滚动到底部
+  container.scrollTop = container.scrollHeight
+}
+function clickButtonPeriodically() {
+  // 获取页面中的所有<li>元素
+  let liElements = document.getElementsByTagName("li")
+
+  // 迭代遍历每个<li>元素
+  for (let i = 0; i < liElements.length; i++) {
+    let liElement = liElements[i]
+
+    // 检查<li>元素的文本内容是否包含满分
+    let liText = liElement.textContent
+
+    if (liText.includes("满分")) {
+      // 触发点击事件
+      liElement.click()
+    }
+  }
+
+  // 使用querySelector选择符合条件的按钮元素
+  let buttonElement = document.querySelector(
+    'button#score[data-event="enter"].button-big.ensure-score'
+  ) as HTMLButtonElement
+
+  // 触发点击事件
+  if (buttonElement) {
+    buttonElement.click()
+  }
+}
+function creatSorceButton() {
+  // 创建按钮元素
+  let button = document.createElement("button")
+  button.className = "rating-button"
+  button.textContent = "一键评分"
+
+  // 添加按钮到容器中
+  document.body.appendChild(button)
+
+  // 添加点击事件处理程序
+  button.addEventListener("click", function () {
+    setTimeout(() => {
+      openSpanMenu()
+      setInterval(() => {
+        clickButtonPeriodically()
+      }, 1000)
+    }, 3000)
+  })
 }
